4292bc1ee956a06ee44125f2293636431e9ec18c
Atanasio Park <pakbongho@gmail.com>
2022-07-18 18:31:59 +0900

    바텀네비게이션 초기 셋팅

---
+++ lib/src/view/pages/near/near_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NearPage extends StatelessWidget {
+  const NearPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('내근처'),
+    ));
+  }
+}
